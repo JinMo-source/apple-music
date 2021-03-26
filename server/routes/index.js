92921d49c46cf5e1a15388620b4044f44329592d
JinMo-source <qkrwslah123@naver.com>
2021-03-26 23:35:07 +0900

    client랑sever 연결

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('http://localhost:3001/api/');
-    res.send({title: 'hello react!'});
+    res.send({greeting: 'hello react!'});
 });
 
 module.exports = router;
